60dcd3f5f40119b5eb53fda6d25737c24f4d7ea6
Zetta Tips <markjue@zettatips.com>
2020-09-16 00:17:18 +0800

    20200916 0016 Showin Score and fix saving the score

--- src/App.js
+++ src/App.js
@@ -42,17 +42,22 @@ export default function App() {
 	];
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(showScore);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
-	const handleAnswerButtonClick = (answerOption) => {
+	const handleAnswerButtonClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+	
 		const nextQuestion = currentQuestion + 1;
-		setCurrentQuestion(nextQuestion);
 
 		if (nextQuestion < questions.length) {
 			setCurrentQuestion(nextQuestion);
 		} else {
 			setShowScore(true);
 		}
+		
 	};
 
 	return (
@@ -60,18 +65,18 @@ export default function App() {
 			{/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
 			{showScore ? (
-				<div className='score-section'>You scored 1 out of {questions.length}</div>
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question 1</span>/{questions.length}
+							<span>Question {currentQuestion  + 1}</span>/{questions.length}
 						</div>
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption, index) => (
-							<button onClick={() => handleAnswerButtonClick()}>{answerOption.answerText}</button>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
 						))}
 					</div>
 				</>
